3210daf25ab2102eb4c97e18ddc34a287e807023
OREO206 <baek6654@naver.com>
2024-03-25 14:51:28 +0900

    2024-03-25 service

---
+++ basic/src/main/java/com/subin/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,12 @@
+package com.subin.basic.service.implement;
+
+import com.subin.basic.service.BasicService;
+
+public class BasicServiceImplement implements BasicService{
+
+    @Override
+    public String getHello() {
+        return "Hello Springboot!!";
+    }
+    
+}
