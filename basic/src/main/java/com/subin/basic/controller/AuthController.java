b737626b803f6e59221b94deaedccf9f26330f2a
OREO206 <baek6654@naver.com>
2024-04-16 15:51:14 +0900

    2024.04.16.

--- basic/src/main/java/com/subin/basic/controller/AuthController.java
+++ basic/src/main/java/com/subin/basic/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.subin.basic.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final BasicService basicService;
-
+    
     @GetMapping("/{principle}")
     public String getJwt(
         @PathVariable("principle") String principle
@@ -30,5 +31,12 @@ public class AuthController {
         @RequestBody String jwt
     ) {
         return basicService.jwtValidate(jwt);
+    }
+
+    @GetMapping("/authentication/principle")
+    public String authenticationPrinciple(
+        @AuthenticationPrincipal String username
+    ) {
+        return "접근 주체 : " + username;
     }
 }
--- basic/src/main/java/com/subin/basic/provider/JwtProvider.java
+++ basic/src/main/java/com/subin/basic/provider/JwtProvider.java
@@ -8,10 +8,6 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.subin.basic.service.BasicService;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -31,11 +27,12 @@ public class JwtProvider {
 
     // JWT 암호화에 사용되는 비밀키는 보안 관리가 되어야 함
     // 코드에 직접적으로 비밀키를 작성하는 것은 보안상 좋지 않음
+
     // 해결책
-    // 1. application.properties / application.yaml에 등록
+    // 1. application.properties / application.yaml 에 등록
     // - application.properties 혹은 application.yaml에 비밀키를 작성
-    // @Value()를 이용하여 데이터를 가져옴
-    // - 주의사항 : application.properties / application.yaml을 .gitignore에 등록해야 함
+    // - @Value()를 이용하여 데이터를 가져옴
+    // - 주의사항 : application.properties / application.yaml을 .gitignore에 등록해야함
     @Value("${jwt.secret-key}")
     private String secretKey;
 
@@ -46,7 +43,7 @@ public class JwtProvider {
     // 3. 외부 데이터 관리 도구를 사용
     // - 해당 서버가 아닌 타 서버에 등록된 Vault 도구를 사용하여 비밀키를 관리
     // - OS 부팅시에 Vault 서버에 접근하여 비밀키를 가져와서 사용하는 방법
-    // - 매 부팅시 다른 비밀키를 제공해 줌
+    // - 매 부팅시 다른 비밀키를 제공해줌
     
     // JWT 생성
     public String create(String principle) {
@@ -73,24 +70,26 @@ public class JwtProvider {
     }
 
     public String validation(String jwt) {
-        
-        // jwt 검증 결과로 나타나는 페이로드가 저장될 변수 
-        Claims claims = null; 
+
+        // jwt 검증 결과로 나타나는 페이로드가 저장될 변수
+        Claims claims = null;
         // 비밀키 생성
         Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         try {
             // 비밀키로 jwt 복호화 작업
             claims = Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(jwt)
-                    .getBody();
-        }catch(Exception exception) {
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+        } catch(Exception exception) {
             exception.printStackTrace();
             return null;
         }
 
         return claims.getSubject();
+
     }
+
 }
