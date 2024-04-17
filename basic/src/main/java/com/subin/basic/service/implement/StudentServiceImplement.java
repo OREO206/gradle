6449e60b4752bdfc7860178d7a90e9c5eab78ce4
OREO206 <baek6654@naver.com>
2024-04-17 16:55:13 +0900

    2024.04.17.

--- basic/src/main/java/com/subin/basic/service/implement/StudentServiceImplement.java
+++ basic/src/main/java/com/subin/basic/service/implement/StudentServiceImplement.java
@@ -1,6 +1,7 @@
 package com.subin.basic.service.implement;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ public class StudentServiceImplement implements StudentService {
 
     // PasswordEncoder 인터페이스 :
     // - Spring Security에서 제공해주는 비밀번호를 안전하게 관리하고 검증하도록 도움을 주는 인터페이스
-    // - String encode(평문패스워드) : 평문 패스워드를 암호화해서 반환
+    // - String encode(평문패스워드) : 평문 패스워드를 암호화해서 반환함
     // - boolean matches(평문패스워드, 암호화된패스워드) : 평문 패스워드와 암호화된 패스워드가 같은지 비교 결과를 반환
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -32,16 +33,16 @@ public class StudentServiceImplement implements StudentService {
 
         // 패스워드 암호화 작업
         String password = dto.getPassword();
-        String encodePassword = passwordEncoder.encode(password);
+        String encodedPassword = passwordEncoder.encode(password);
 
-        dto.setPassword(encodePassword);
+        dto.setPassword(encodedPassword);
 
         // CREATE (SQL : INSERT)
         // 1. Entity 클래스의 인스턴스 생성
         // 2. 생성한 인스턴스를 repository.save() 메서드로 저장
         StudentEntity studentEntity = new StudentEntity(dto);
         // save() : 저장 및 수정 (덮어쓰기)
-        studentRepository.save(studentEntity);
+        StudentEntity savedEntity = studentRepository.save(studentEntity);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("성공!");
     }
@@ -52,10 +53,11 @@ public class StudentServiceImplement implements StudentService {
         Integer studentNumber = dto.getStudentNumber();
         String address = dto.getAddress();
 
-        // 0. student 테이블에 해당하는 Primary key를 가지는 레코드가 존재하는지 확인
+        // 0. student 테이블에 해당하는 Prmary key를 가지는 레코드가 존재하는지 확인
         boolean isExistedStudent = studentRepository.existsById(studentNumber);
-        if(!isExistedStudent) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 학생입니다.");
+        if (!isExistedStudent) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 학생입니다.");
 
+        // UPDATE (SQL : UPDATE)
         // 1. student 클래스로 접근 (StudentRepository 사용)
         StudentEntity studentEntity = studentRepository.
         // 2. dto.studentNumber에 해당하는 인스턴스를 검색
@@ -71,9 +73,10 @@ public class StudentServiceImplement implements StudentService {
 
     @Override
     public ResponseEntity<String> deleteStudent(Integer studentNumber) {
-
-        studentRepository.deleteById(studentNumber);
         
+        // DELETE (SQ : DELETE)
+        studentRepository.deleteById(studentNumber);
+
         return ResponseEntity.status(HttpStatus.OK).body("성공");
     }
 
@@ -81,19 +84,19 @@ public class StudentServiceImplement implements StudentService {
     public ResponseEntity<String> signIn(SignInRequestDto dto) {
         
         try {
-
+            
             Integer studentNumber = dto.getStudentNumber();
             StudentEntity studentEntity = studentRepository.findByStudentNumber(studentNumber);
 
-            if(studentEntity == null)
+            if (studentEntity == null) 
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("오류");
-                
+
             // 사용자가 입력한 패스워드와 암호화된 패스워드가 매치되는지 확인
             String password = dto.getPassword();
             String encodedPassword = studentEntity.getPassword();
 
             boolean isEqualPassword = passwordEncoder.matches(password, encodedPassword);
-            if(!isEqualPassword)
+            if (!isEqualPassword) 
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비밀번호 불일치");
 
         } catch(Exception exception) {
@@ -102,12 +105,7 @@ public class StudentServiceImplement implements StudentService {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body("성공");
-    }
 
-// CRUD
-// CREATE: 1~2
-// READ: 엄청 많음
-// UPDATE: 1 ~ 컬럼 개수
-// DELETE: 1 ~ 2
+    }
     
 }
